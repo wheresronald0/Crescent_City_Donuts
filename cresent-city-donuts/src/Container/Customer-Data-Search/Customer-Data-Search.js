1acfd9163b09ac1f00e633be68356b0df187bd37
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-12-13 15:30:01 -0500

    customer search component styled

--- cresent-city-donuts/src/Container/Customer-Data-Search/Customer-Data-Search.js
+++ cresent-city-donuts/src/Container/Customer-Data-Search/Customer-Data-Search.js
@@ -11,7 +11,7 @@ import {
   ControlLabel,
   FormControl
 } from "react-bootstrap";
-import backgroundLight from "../../Assests/background3.jpg";
+import backgroundLight from "../../Assests/background3-opacity.jpg";
 
 class CustomerDataSearch extends Component {
   constructor(props) {
@@ -43,33 +43,48 @@ class CustomerDataSearch extends Component {
     const searchResults = this.state.responseData;
     const eachResult = searchResults.map(customers => {
       return (
-        <div key={customers._id}>
-          {customers.firstName}&nbsp;
-          {customers.lastName}&nbsp;
-          {customers.street}&nbsp;
-          {customers.city}&nbsp;
-          {customers.state}&nbsp;
-          {customers.zip}&nbsp;
-          {customers.email}&nbsp;
-          <div>
-            <NavLink to={"/customer/" + customers._id}>
-              <Button onClick={event => this.props.onCustomerSelect(customers)}>
-                Select Customer
-              </Button>
-            </NavLink>
+        <div>
+          <div key={customers._id} className="mappedSearchResults">
+            <strong>Customer Name: &nbsp;</strong>
+            {customers.firstName}&nbsp;
+            {customers.lastName}&nbsp; | <strong>Address: &nbsp;</strong>
+            {customers.street}&nbsp;
+            {customers.city},&nbsp;
+            {customers.state}&nbsp;
+            {customers.zip}&nbsp; | &nbsp; <strong>Email: &nbsp;</strong>{" "}
+            {customers.email}&nbsp;
+            <div>
+              <NavLink to={"/customer/" + customers._id}>
+                <Button
+                  bsStyle="info"
+                  onClick={event => this.props.onCustomerSelect(customers)}
+                >
+                  Select Customer
+                </Button>
+              </NavLink>
+            </div>
           </div>
         </div>
       );
     });
 
     return (
-      <div className="searchFormContainer">
+      <div
+        className="searchFormContainer"
+        style={{
+          backgroundImage: "url(" + backgroundLight + ")",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat"
+          // opacity: "0.5"
+        }}
+      >
         <div>
           <Form inline className="customerLookupForm">
             <FormGroup controlId="formInlineName">
-              <ControlLabel>
-                Customer Lookup by First Name, Last Name, or Email{" "}
-              </ControlLabel>{" "}
+              <ControlLabel id="searchTitle">
+                Lookup Customer by First Name, Last Name, or Email{" "}
+              </ControlLabel>
               <FormControl
                 type="text"
                 placeholder="Jane Doe"
@@ -77,7 +92,7 @@ class CustomerDataSearch extends Component {
                   this.setState({ searchParameter: event.target.value })
                 }
               />
-            </FormGroup>{" "}
+            </FormGroup>
             <Button bsStyle="warning" onClick={this.customerSearchHandler}>
               Search for Customer
             </Button>
@@ -88,19 +103,9 @@ class CustomerDataSearch extends Component {
               <Button bsStyle="warning">Back</Button>
             </NavLink>
           </Form>
-          {/* <hr id="searchHR" /> */}
         </div>
 
-        <div
-          className="seachContainer"
-          style={{
-            backgroundImage: "url(" + backgroundLight + ")",
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat"
-            // opacity: "0.5"
-          }}
-        >
+        <div className="seachContainer">
           <ul>{eachResult}</ul>
         </div>
       </div>
